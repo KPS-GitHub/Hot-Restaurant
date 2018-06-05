101bd909861668c262f31a8c17e35c34f777748b
Kennan <kennan.smith1@gmail.com>
2018-06-05 15:18:26 -0600

    started code for table-data.js and waiting-list.js

---
+++ app/data/waitinglist.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+    {
+        customerName: "Kennan",
+        customerEmail: "kennan@example.com",
+        customerID: "kennan and co",
+        phoneNumber: "000-000-0001"
+    }
+];
+
+module.exports = waitingArray;
