9b8170b663e7658fd8c4486207888c0ebf6bcddd
Kennan <kennan.smith1@gmail.com>
2018-06-05 15:08:43 -0600

    got server.js up and running; filled out file structure

---
+++ server.js
@@ -0,0 +1,29 @@
+// express makes routing easy via nodejs
+var express = require('express');
+// body-parser allows us to receive info in json
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// default route
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(function (req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('you posted:\n');
+    res.end(JSON.stringify(req.body, null, 2));
+});
+
+app.listen(PORT, function(){
+    console.log("listening on PORT: " + PORT);
+});
