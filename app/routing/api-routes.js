ea45580faf053b6edb4be60230d39c871dcc01e1
Kennan Smith <kennan.smith1@gmail.com>
2018-06-06 08:47:35 -0600

    added waitlist api route; filled out tables.html and got it running properly

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -6,5 +6,9 @@ module.exports = function(app) {
     app.get("/api/tables", function(req, res) {
         res.json(tableData);
     });
-    
+
+    app.get("/api/waitlist", function(req, res) {
+        res.json(waitingListData);
+    });
+
 };
